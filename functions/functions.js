e5aaa4064333374e7d39d65a3671dac94886c669
Nir Kahana <nirkahana8@gmail.com>
2020-10-01 18:12:59 +0300

    Create functions.js

---
+++ functions/functions.js
@@ -0,0 +1,13 @@
+
+const multiple = (x,y) => x*y;
+
+const divide = (x,y) => x/y;
+
+const sum = (x,y) => x+y;
+
+const subtract = (x,y) => x-y;
+
+const root = (x) => Math.sqrt(x);
+
+const square = (x) => x*x;
+
